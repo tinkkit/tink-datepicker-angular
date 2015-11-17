73b962800c9b8b79b07f36a936c69db677478489
Lieven VG <lievenvg@gmail.com>
2015-11-17 11:47:31 +0100

    dist

--- dist/tink-datepicker-angular.js
+++ dist/tink-datepicker-angular.js
@@ -466,7 +466,7 @@
               // labels for the days you can make this variable //
               var dayLabels = ['ma', 'di', 'wo', 'do', 'vr', 'za', 'zo'];
               // -- create the labels  --/
-              scope.labels = [];
+              scope.dayLabels = $sce.trustAsHtml('<th>' + dayLabels.join('</th><th>') + '</th>');
               // Add a watch to know when input changes from the outside //
 
               // -- check if we are using a touch device  --/
@@ -574,7 +574,7 @@
                 $event.preventDefault(); return false;
               };
 
-            
+
 
               scope.$select = function (date) {
                 //addTime(date,scope.ngModel);
@@ -621,12 +621,13 @@
                 if ($directive.mode === 0) {
                   scope.title = dateCalculator.format($directive.viewDate, options.yearTitleFormat);
                   scope.rows = calView.daysInRows($directive.viewDate, $directive.selectedDate, scope.minDate, scope.maxDate);
-                  scope.labels = $sce.trustAsHtml('<th>' + dayLabels.join('</th><th>') + '</th>');
+                  scope.showLabels = $sce.trustAsHtml('<th>' + dayLabels.join('</th><th>') + '</th>');
                 }
                 if ($directive.mode === 2) {
                   var currentYear = parseInt(dateCalculator.format($directive.viewDate, 'yyyy'));
                   scope.title = (currentYear - 11) + '-' + currentYear;
                   scope.rows = calView.yearInRows($directive.viewDate, scope.minDate, scope.maxDate);
+                  scope.showLabels = 0;
                     //setMode(1);
                   }
                 };
@@ -700,11 +701,12 @@
           }
         };
       }]);
-    })();;angular.module('tink.datepicker').run(['$templateCache', function($templateCache) {
+    })();
+;angular.module('tink.datepicker').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('templates/tinkDatePickerField.html',
-    "<div role=datepicker class=\"dropdown-menu datepicker\" ng-class=\"{'aligns-right': $alignsright == 'true'}\"> <table style=\"table-layout: fixed; height: 100%; width: 100%\"> <thead> <tr class=text-center> <th> <button tabindex=-1 type=button ng-disabled=pane.prev aria-label=\"vorige maand\" class=\"btn pull-left\" ng-mousedown=$disable($event) ng-click=$selectPane(-1)> <i class=\"fa fa-chevron-left\"></i> </button> </th> <th colspan=\"{{ rows[0].length - 2 }}\"> <button tabindex=0 type=button class=\"btn btn-block text-strong\" ng-mousedown=$disable($event) ng-click=$toggleMode()> <strong style=\"text-transform: capitalize\" ng-bind=title></strong> </button> </th> <th> <button tabindex=0 type=button ng-mousedown=$disable($event) ng-disabled=pane.next aria-label=\"volgende maand\" class=\"btn pull-right\" ng-click=$selectPane(+1)> <i class=\"fa fa-chevron-right\"></i> </button> </th> </tr> <tr class=datepicker-days ng-bind-html=labels ng-if=showLabels></tr> </thead> <tbody> <tr ng-repeat=\"(i, row) in rows\" height=\"{{ 100 / rows.length }}%\"> <td class=text-center ng-repeat=\"(j, el) in row\"> <button tabindex=0 type=button class=btn style=\"width: 100%\" ng-class=\"{'btn-selected': el.selected, 'btn-today': el.isToday && !el.elected, 'btn-grayed':el.isMuted}\" ng-mousedown=$disable($event) ng-focus=elemFocus($event) ng-click=$select(el.date) ng-disabled=el.disabled> <span role=\"\" ng-class=\"{'text-muted': el.muted}\" ng-bind=el.label></span> </button> </td> </tr> </tbody> </table> </div>"
+    "<div role=datepicker class=\"dropdown-menu datepicker\" ng-class=\"{'aligns-right': $alignsright == 'true'}\"> <table style=\"table-layout: fixed; height: 100%; width: 100%\"> <thead> <tr class=text-center> <th> <button tabindex=-1 type=button ng-disabled=pane.prev aria-label=\"vorige maand\" class=\"btn pull-left\" ng-mousedown=$disable($event) ng-click=$selectPane(-1)> <i class=\"fa fa-chevron-left\"></i> </button> </th> <th colspan=\"{{ rows[0].length - 2 }}\"> <button tabindex=0 type=button class=\"btn btn-block text-strong\" ng-mousedown=$disable($event) ng-click=$toggleMode()> <strong style=\"text-transform: capitalize\" ng-bind=title></strong> </button> </th> <th> <button tabindex=0 type=button ng-mousedown=$disable($event) ng-disabled=pane.next aria-label=\"volgende maand\" class=\"btn pull-right\" ng-click=$selectPane(+1)> <i class=\"fa fa-chevron-right\"></i> </button> </th> </tr> <tr class=datepicker-days ng-bind-html=dayLabels ng-if=showLabels></tr> </thead> <tbody> <tr ng-repeat=\"(i, row) in rows\" height=\"{{ 100 / rows.length }}%\"> <td class=text-center ng-repeat=\"(j, el) in row\"> <button tabindex=0 type=button class=btn style=\"width: 100%\" ng-class=\"{'btn-selected': el.selected, 'btn-today': el.isToday && !el.elected, 'btn-grayed':el.isMuted}\" ng-mousedown=$disable($event) ng-focus=elemFocus($event) ng-click=$select(el.date) ng-disabled=el.disabled> <span role=\"\" ng-class=\"{'text-muted': el.muted}\" ng-bind=el.label></span> </button> </td> </tr> </tbody> </table> <div class=go-to-today> <button type=button class=btn-sm>Vandaag</button> </div> </div>"
   );
 
 
